fa0df411d8d65cfd273bbd6a0fa57d9f16ab784e
Vadim Ripa <seemsod1@gmail.com>
2024-06-16 23:32:21 +0300

    added tests for config

--- internal/config/config.go
+++ internal/config/config.go
@@ -1,19 +1,20 @@
 package config
 
 import (
-	"log"
-
 	"github.com/kelseyhightower/envconfig"
 )
 
 // AppConfig is a struct that holds the configuration of the app
-type AppConfig struct{}
+type AppConfig struct {
+	Prod bool `required:"true"`
+}
 
 // NewAppConfig creates a new AppConfig
-func NewAppConfig() *AppConfig {
+func NewAppConfig() (*AppConfig, error) {
 	var appConfig AppConfig
-	if err := envconfig.Process("app", &appConfig); err != nil {
-		log.Fatal(err)
+	err := envconfig.Process("app", &appConfig)
+	if err != nil {
+		return nil, err
 	}
-	return &appConfig
+	return &appConfig, nil
 }
---
+++ internal/config/config_test.go
@@ -0,0 +1,26 @@
+package config_test
+
+import (
+	"os"
+	"testing"
+
+	"github.com/seemsod1/api-project/internal/config"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNewAppConfig_Success(t *testing.T) {
+	os.Setenv("APP_PROD", "true")
+	defer os.Unsetenv("APP_PROD")
+
+	appConfig, err := config.NewAppConfig()
+	require.NoError(t, err)
+	require.NotNil(t, appConfig)
+	require.True(t, appConfig.Prod)
+}
+
+func TestNewAppConfig_Failure(t *testing.T) {
+	os.Clearenv()
+	appConfig, err := config.NewAppConfig()
+	require.Nil(t, appConfig)
+	require.Error(t, err)
+}
